5218636f7125154d24161143439287c49e7cb6f6
tndus73 <91958363+tndus73@users.noreply.github.com>
2022-10-06 14:00:06 +0900

    img파일 사이트에 올리기

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+from django.urls import path
+from . import views
+
 urlpatterns = [
  #path('', views.homepage, name='homepage'),
  path('home/', views.home),
