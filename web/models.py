9a14690ba5cc059d81ea9953594429ed48883dc3
Mingu_Kang <alsrn5190@gmail.com>
2022-10-12 17:42:10 +0900

    id 수정

--- web/models.py
+++ web/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class citytourdata(models.Model):
+class Citytourdata(models.Model):
+    id = models.IntegerField(primary_key=True)
     city1 = models.CharField(max_length=50)
     city2 = models.CharField(max_length=50)
     cos_name = models.CharField(max_length=50)
-    cos_info = models.CharField(max_length=200, null=True)
-    start_time = models.CharField(max_length=50, null=True)
-    close_time = models.CharField(max_length=50, null=True)
-    url = models.CharField(max_length=200, null=True)
-    number = models.CharField(max_length=50, null=True)
+    cos_info = models.CharField(max_length=200, blank=True, null=True)
+    start_time = models.CharField(max_length=50, blank=True, null=True)
+    close_time = models.CharField(max_length=50, blank=True, null=True)
+    url = models.CharField(max_length=200, blank=True, null=True)
+    number = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.city1 +","+self.city2 +","+self.cos_name+\
+            ","+self.cos_info+","+self.start_time+","+self.close_time+","+self.url+","+self.number
