8324e16606dcd79249be63552d82ff36e00a0f88
Mingu_Kang <alsrn5190@gmail.com>
2022-10-17 09:16:16 +0900

    DB, 검색 템플릿

--- web/models.py
+++ web/models.py
@@ -9,13 +9,16 @@ from django.db import models
 
 # Create your models here.
 class Citytourdata(models.Model):
-    id = models.IntegerField(primary_key=True)
-    city1 = models.CharField(max_length=50)
-    city2 = models.CharField(max_length=50)
-    cos_name = models.CharField(max_length=50)
-    cos_info = models.CharField(max_length=200, blank=True, null=True)
-    start_time = models.CharField(max_length=50, blank=True, null=True)
-    close_time = models.CharField(max_length=50, blank=True, null=True)
-    url = models.CharField(max_length=200, blank=True, null=True)
-    number = models.CharField(max_length=50, blank=True, null=True)
+    city1       = models.CharField(max_length=50)
+    city2       = models.CharField(max_length=50)
+    cos_name    = models.CharField(max_length=50)
+    cos_info    = models.CharField(max_length=200, blank=True, null=True)
+    start_time  = models.CharField(max_length=50, blank=True, null=True)
+    close_time  = models.CharField(max_length=50, blank=True, null=True)
+    url         = models.CharField(max_length=200, blank=True, null=True)
+    number      = models.CharField(max_length=50, blank=True, null=True)
+    id          = models.IntegerField(primary_key=True)
+
+    def __str__(self):
+        return self.number+","+self.city2+","+self.city2+","+self.cos_name+","+self.cos_info+","+self.start_time+","+self.close_time+","+self.url+","+self.number
 
--- web/views.py
+++ web/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -7,15 +5,36 @@ from django.http import HttpResponse
 from django.template import loader
 import time
 from datetime import datetime
+from .models import Citytourdata
+
 
 def homepage(request) :
  return HttpResponse("<h1>홈페이지 만들어봅시다!!</h1>")
 
 def home(request) :
- return render(request, 'home.html')
+ city1 = Citytourdata.objects.values('city1').distinct()
+ city1_1=[]
+ for i in range(0,len(city1),1):
+  a = city1[i]
+  city1_1.append(a.get('city1'))
+
+ city2 = Citytourdata.objects.values('city2').distinct()
+ city2_1=[]
+ for i in range(0,len(city2),1):
+  b = city2[i]
+  city2_1.append(b.get('city2'))
+
+ context = {
+  'city1': city1_1,
+  'city2': city2_1,
+ }
+
+ return render(request,"home.html", context)
+
 
 def header(request):
  return render(request, 'header.html')
 
 def introduction(request):
  return render(request,"introduction.html")
+
