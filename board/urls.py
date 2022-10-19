701f40dd3253138869a05912153f43df7413fe8f
tndus73 <91958363+tndus73@users.noreply.github.com>
2022-10-19 14:15:30 +0900

    게시판 기능

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'board'
+
 urlpatterns = [
     path('board/', views.board),
+    path('board/<int:pk>/', views.posting, name="posting"),
+    path('board/writing/', views.writing),
 ]
