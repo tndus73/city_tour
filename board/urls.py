d66097b117bafad4b45eacfbb62ff5a173d67590
tndus73 <91958363+tndus73@users.noreply.github.com>
2022-10-26 15:30:04 +0900

    url인코딩

--- board/urls.py
+++ board/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'board'
 
 urlpatterns = [
-    path('board/', views.board),
+    path('board/', views.board, name="board"),
     path('board/<int:pk>/', views.posting, name="posting"),
-    path('board/writing/', views.writing),
+    path('board/writing/', views.writing, name="writing"),
     path('board/<int:post_id>/remove/', views.remove, name="remove"),
     path('board/<int:post_id>/update/', views.update, name="update"),
 ]
--- web/urls.py
+++ web/urls.py
@@ -7,11 +7,12 @@ from django.contrib import admin
 
 urlpatterns = [
  #path('', views.homepage, name='homepage'),
- path('home/', views.home),
- path('homeCity/', views.homecity),
- path('header/', views.header),
- path('introduction/', views.introduction),
- path('manager/', views.manager),
- path('selectCity/', views.selectCity),
- path('whole_country/', views.whole_country),
+ path('home/', views.home, name='home'),
+ path('homeCity/', views.homecity, name='homeCity'),
+ path('header/', views.header, name='header'),
+ path('introduction/', views.introduction, name='introduction'),
+ path('manager/', views.manager, name='manager'),
+ path('selectCity/', views.selectCity, name='selectCity'),
+ path('whole_country/', views.whole_country, name='whole_country'),
+
 ]
